2d9fcb0665a0d2f43f60e09dfb1f2b6bb261cc47
HafizhSP <hafizhsalsabilap@gmail.com>
2019-04-27 11:15:18 +0700

    Remidi UTS PBO

---
+++ src/uts/Komputer.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uts;
+
+/**
+ *
+ * @author Hafizh
+ */
+public class Komputer {
+    private CPU cpu;
+    
+    public Komputer(CPU cpu){
+        this.cpu = cpu;
+    }
+
+    public Komputer(){
+    }
+
+    public void memasang(CPU cpu){
+        this.cpu=cpu;
+        System.out.println("CPU: " + this.cpu.getCPUData() + " GHz " + "dicabut");
+    }
+
+    public void mencabut(CPU cpu){
+        this.cpu=cpu;
+        System.out.println("CPU: " + this.cpu.getCPUData() + " GHz " + "dipasang");
+    }
+    
+    public void cetakInfo(){
+        System.out.println("Spesifikasi: CPU:" + this.cpu.getCPUData() + " GHz");
+    }
+
+}
